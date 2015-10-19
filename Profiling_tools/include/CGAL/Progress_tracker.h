53e3f99c60ed261eb00fca83e3024d29da7dc7f5
Simon Giraudot <simon.giraudot@geometryfactory.com>
2015-10-19 11:44:53 +0200

    Ascii bar tracker

--- Profiling_tools/include/CGAL/Progress_tracker.h
+++ Profiling_tools/include/CGAL/Progress_tracker.h
@@ -45,6 +45,50 @@ public:
 
 };
 
+template <typename Observed>
+class Ascii_bar_progress_tracker : public Abstract_progress_tracker<Observed>
+{
+private:
+
+  unsigned int m_refresh_time;
+  unsigned int m_latest;
+  unsigned int m_width;
+  
+public:
+
+  Ascii_bar_progress_tracker (time_t refresh_time = 1, // Default = update every 1 second
+                              unsigned int width = 50)  // Width of progress bar
+    : m_refresh_time (refresh_time), m_width (width)
+  {
+    m_latest = time (NULL);
+  }
+  virtual ~Ascii_bar_progress_tracker () { }
+  
+  virtual void notify (const Observed* obs)
+  {
+    unsigned int current = time (NULL);
+    
+    if (current < m_latest + m_refresh_time)
+      return;
+
+    double done = obs->progress ();
+    unsigned int bars_filled = (unsigned int)(done * m_width);
+    
+    std::cerr << "\r[";
+    unsigned int i = 0;
+    for (; i < bars_filled; ++ i)
+      std::cerr << "=";
+    for (; i < m_width; ++ i)
+      std::cerr << " ";
+    
+    std::cerr << "] " << (unsigned int)(100. * done) << "%";
+    
+
+    m_latest = time (NULL);
+  }
+
+};
+
 
 
 } // namespace CGAL
