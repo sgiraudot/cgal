9508001ff3d4793173b96faf0aa3b7f797339fe1
Simon Giraudot <simon.giraudot@geometryfactory.com>
2015-10-21 14:36:47 +0200

    Additional comments on doc

--- Miscellany/doc/Miscellany/CGAL/Progress_tracker.h
+++ Miscellany/doc/Miscellany/CGAL/Progress_tracker.h
@@ -54,8 +54,21 @@ class Simple_progress_tracker : public Abstract_progress_tracker<Observed>
 {
 public:
 
+/// \name Constructor
+/// @{
+
+/*!
+  \param refresh_time The progress state in only displayed if the last
+  display happened more than this time ago.
+
+
+  \warning Using `refresh_time=0` means there is no limitation on the
+  rate of display. This can badly affect performances.
+
+*/
   Simple_progress_tracker (time_t refresh_time = 1); // Default = update every 1 second
-  virtual void notify (const Observed* obs);
+
+/// @}
 };
 
 /*!
@@ -76,12 +89,23 @@ template <typename Observed>
 class Ascii_bar_progress_tracker : public Abstract_progress_tracker<Observed>
 {
 public:
+/// \name Constructor
+/// @{
 
+/*!
+  \param refresh_time The progress state in only displayed if the last
+  display happened more than this time ago.
+
+  \warning Using `refresh_time=0` means there is no limitation on the
+  rate of display. This can badly affect performances.
+
+  \param width Total number of characters of the ASCII progress bar.
+
+*/
   Ascii_bar_progress_tracker (time_t refresh_time = 1, // Default = update every 1 second
                               unsigned int width = 50);  // Width of progress bar
-  
-  virtual void notify (const Observed* obs);
 
+/// @}
 };
 
 /*!
@@ -105,14 +129,61 @@ class Abstract_remaining_time_progress_tracker : public Abstract_progress_tracke
 
 public:
 
+/// \name Abstract callback
+/// @{
+
+/*!
+  This method is called by the `Observed` object when its progress has
+  changed.
+
+  \warning The `Observed` object calls this method whenever its
+  progress has changed. It is the responsability of the
+  `Progress_tracker` to make sure it does not call the method
+  `progress()` too often (which can badly impact the performances if
+  `progress()` requires heavy computation).
+*/
   virtual void notify (const Observed* obs) = 0;
 
+/// @}
+
+/// \name Useful methods
+/// @{
+
+/*!
+  Estimates the expected remaining time before the algorithm is
+  finished.
+
+  \param time_done The number of seconds ellapsed since the algorithm
+  started.
+
+  \param fraction_done The state of progress of the algorithm (0.8 if
+  the algorithm is 80% complete, etc.).
+
+  \note This function estimates the remaining time by assuming that
+  the time is linear with the progress of the function. The estimation
+  might not be accurate if this is not the case (for example, if the
+  last steps of the algorithm are considerably longer than the first
+  ones).
+*/
   time_t remaining_time (time_t time_done,
                          double fraction_done) const;
 
+/*!
+
+  Formats a time given in a number of seconds in a more readable
+  fashion. `3756 seconds` are displayed as `1h 2min 36sec`.
+
+  \tparam Stream The type of stream where time is displayed.
+
+  \param stream The stream where time is displayed.
+  \param seconds The number of seconds to be formatted.
+
+*/
   template <typename Stream>
   void display_time (Stream& stream, time_t seconds) const;
 
+/// @}
+  
 };
 
 /*!
@@ -135,9 +206,23 @@ class Simple_remaining_time_progress_tracker
 {
 public:
 
-  Simple_remaining_time_progress_tracker (time_t refresh_time = 1); // Default = update every 1 second
+
+/// \name Constructor
+/// @{
   
-  virtual void notify (const Observed* obs);
+/*!
+  \param refresh_time The progress state in only displayed if the last
+  display happened more than this time ago.
+
+
+  \warning Using `refresh_time=0` means there is no limitation on the
+  rate of display. This can badly affect performances.
+
+*/
+
+  Simple_remaining_time_progress_tracker (time_t refresh_time = 1); // Default = update every 1 second
+
+/// @}
 };
 
 /*!
@@ -161,9 +246,23 @@ class Ascii_bar_with_remaining_time_progress_tracker
 {
 public:
 
+/// \name Constructor
+/// @{
+
+/*!
+  \param refresh_time The progress state in only displayed if the last
+  display happened more than this time ago.
+
+  \warning Using `refresh_time=0` means there is no limitation on the
+  rate of display. This can badly affect performances.
+
+  \param width Total number of characters of the ASCII progress bar.
+
+*/
   Ascii_bar_with_remaining_time_progress_tracker (time_t refresh_time = 1, // Default = update every 1 second
                                                   unsigned int width = 30);  // Width of progress bar
-  virtual void notify (const Observed* obs);
+
+/// @}
 
 };
 
